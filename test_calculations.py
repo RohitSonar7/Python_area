4233b6cad34c041334f9edb81f76e4ac7ef96646
unknown <sonarrohit7@gmail.com>
2022-03-28 16:40:58 +0530

    Commit

--- test_calculations.py
+++ test_calculations.py
@@ -1,15 +1,20 @@
 import Calculations
+import pytest
 
 def test_RectangleArea():
     x = 10
     y = 20
     result = Calculations.RectangleArea(x , y)
     assert x * y == result
+
+@pytest.mark.myproject
 def test_ReactanglePeri():
     x = 10
     y = 20
     result = Calculations.RectanglePeri(x , y)
     assert  x + x + y + y == result
+
+@pytest.mark.myproject
 def test_SquareArea():
     x = 10
     result = Calculations.SquareArea(x)
